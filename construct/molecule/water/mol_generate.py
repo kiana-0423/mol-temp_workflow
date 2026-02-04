77dfac053fc33d506862c537ee078535a0152916
kiana-0424 <huang1997c@gmail.com>
2026-02-04 15:28:11 +0900

    update

--- construct/molecule/water/mol_generate.py
+++ construct/molecule/water/mol_generate.py
@@ -3,9 +3,9 @@ from rdkit.Chem import AllChem
 from ase.io import read, write
 
 
-mol = Chem.MolFromSmiles('O')  # dodecane
+mol = Chem.MolFromSmiles('C(C(C)C1=CC=C2C(=C1)C=CC=C2C(CCCCCCCCCCCC)C)CCCCCCCCCCC')  # dodecane
 mol = Chem.AddHs(mol)
 AllChem.EmbedMolecule(mol, AllChem.ETKDG())
-Chem.MolToMolFile(mol, 'water.mol')
-ase_mol = read('water.mol')
-ase_mol.write('water.pdb')
+Chem.MolToMolFile(mol, '7_c.mol')
+ase_mol = read('7_c.mol')
+ase_mol.write('7_c.pdb')
